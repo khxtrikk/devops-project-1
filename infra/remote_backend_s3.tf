6c804d11fa4b17e8ac70dd520ccba4a456f927c5
khxtrikk <khatrisakshi3003@gmail.com>
2025-11-11 10:57:03 +0530

    region fixes

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "your-unique-prefix-terraform-remote-state-12345" # Your unique S3 bucket name
     key            = "dev-proj-1/terraform.tfstate"   # The path where the state file will be stored
-    region         = "eu-central-1"                   # Your AWS region
+    region         = "eu-west-1"                   # Your AWS region
     dynamodb_table = "terraform-locks"                # For state locking (optional)
     encrypt        = true                             # Enable state file encryption
   }
