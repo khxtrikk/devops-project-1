6adcac2d6fc0153fac5492edd302799ab5d3ec12
khxtrikk <khatrisakshi3003@gmail.com>
2025-11-11 11:56:19 +0530

    Remove provider.tf and remote_backend_s3.tf files from Git repository

--- infra/remote_backend_s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-unique-prefix-terraform-remote-state-12345" # Your unique S3 bucket name
-    key            = "dev-proj-1/terraform.tfstate"   # The path where the state file will be stored
-    region         = "eu-west-1"                   # Your AWS region
-    dynamodb_table = "terraform-locks"                # For state locking (optional)
-    encrypt        = true                             # Enable state file encryption
-  }
-}
